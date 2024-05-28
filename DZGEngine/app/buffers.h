5410070893e0b9619bceb4e8886f519e6e1da91e
zemi-taj-fromaz <hrco.ljubas@gmail.com>
2024-05-28 23:50:46 +0200

    ortographic projection

--- DZGEngine/app/buffers.h
+++ DZGEngine/app/buffers.h
@@ -74,10 +74,15 @@ void dzg::updateUniformBuffer(uint32_t currentImage) {
     auto currentTime = std::chrono::high_resolution_clock::now();
     float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
     UniformBufferObject ubo{};
-    ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    ubo.proj = glm::perspective(glm::radians(45.0f), core.swapChainExtent.width / (float)core.swapChainExtent.height, 0.1f, 10.0f);
-    ubo.proj[1][1] *= -1;
+ //   ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    ubo.model = glm::mat4(1.0f);
+
+    ubo.view = glm::mat4(1.0f);// glm::lookAt(glm::vec3(0.0f, 0.0f, .0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+   // ubo.proj = glm::perspective(glm::radians(45.0f), core.swapChainExtent.width / (float)core.swapChainExtent.height, 0.1f, 10.0f);
+
+    ubo.proj = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, -10.0f, 10.0f);
+   // ubo.proj[1][1] *= -1;
 
     memcpy(uniformBuffersMapped[currentImage], &ubo, sizeof(ubo));
 
