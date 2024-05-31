8c4731cb74e647a764bef0a32e94ae6b00699af8
zemi-taj-fromaz <hrco.ljubas@gmail.com>
2024-05-31 11:49:02 +0200

    tidy up app creation

--- ExeApp/my_scene.h
+++ ExeApp/my_scene.h
@@ -15,6 +15,16 @@ class my_scene : public scene
 public:
 	my_scene()
 	{
+
+		//STEPS
+		// 1) Textures
+		// 2) Samplers
+		// 3) Bindings
+		// 4) Descriptor set layouts
+		// 5) BufferData
+		// 6) PipelineData
+		// 7) Descriptor set
+		// 8) Mesh
 		auto texSampler = std::make_shared<sampler>();
 
 		std::shared_ptr<texture> tex = std::make_shared<texture>();
@@ -23,7 +33,7 @@ public:
 		BindingInfo bInfo1(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,  VK_SHADER_STAGE_VERTEX_BIT);
 		BindingInfo bInfo2(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,  VK_SHADER_STAGE_FRAGMENT_BIT);
 
-		BufferData* bufferData = new BufferData();
+		std::shared_ptr<BufferData> bufferData = std::make_shared<BufferData>();
 		bufferData->type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 		bufferData->usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
 		bufferData->size = sizeof(UniformBufferObject);
@@ -38,22 +48,22 @@ public:
 				memcpy(bufferMapped, &ubo, sizeof(ubo));
 			};
 
-		BufferData* imageData = new BufferData();
+		std::shared_ptr<BufferData> imageData = std::make_shared<BufferData>();
 		imageData->type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
 	//	imageData->size = sizeof(UniformBufferObject);
 		imageData->is_image = true;
 		imageData->Texture = tex;
 		imageData->TextureSampler = texSampler;
 
-		DescriptorSetLayout* dsl = new DescriptorSetLayout();
+		std::shared_ptr<DescriptorSetLayout> dsl = std::make_shared<DescriptorSetLayout>();
 		dsl->bindings.push_back(bInfo1);
 		dsl->bindings.push_back(bInfo2);
 
 		std::shared_ptr<PipelineData> pData = std::make_shared<PipelineData>();
 		pData->shaderNames = std::array<std::string, 2>({ "TriangleShader.vert", "TriangleShader.frag" });
-		pData->pDescriptorSetLayout = std::shared_ptr<DescriptorSetLayout>(dsl);
+		pData->pDescriptorSetLayout = dsl;
 
-		DescriptorSet* ds = new DescriptorSet();
+		std::shared_ptr<DescriptorSet> ds = std::make_shared<DescriptorSet>();
 		ds->layout = pData->pDescriptorSetLayout;
 		ds->bufferDataVec.push_back(std::shared_ptr<BufferData>(bufferData));
 		ds->bufferDataVec.push_back(std::shared_ptr<BufferData>(imageData));
@@ -61,10 +71,10 @@ public:
 
 
 		pipelineDataVec.push_back(pData);
-		DescriptorSetLayoutVec.push_back(std::shared_ptr<DescriptorSetLayout>(dsl));
-		BufferDataVec.push_back(std::shared_ptr<BufferData>(bufferData));
-		BufferDataVec.push_back(std::shared_ptr<BufferData>(imageData));
-		DescriptorSetVec.push_back(std::shared_ptr<DescriptorSet>(ds));
+		DescriptorSetLayoutVec.push_back(dsl);
+		BufferDataVec.push_back(bufferData);
+		BufferDataVec.push_back(imageData);
+		DescriptorSetVec.push_back(ds);
 		TexturesVec.push_back(tex);
 		SamplersVec.push_back(texSampler);
 
@@ -72,13 +82,6 @@ public:
 		m.DescriptorSetVec = DescriptorSetVec;
 		m.PipelineData = pData;
 		MeshVec.push_back(m);
-		
-
-		pData = nullptr;
-		dsl = nullptr;
-		ds = nullptr;
-		bufferData = nullptr;
-		imageData = nullptr;
 	}
 };
 
